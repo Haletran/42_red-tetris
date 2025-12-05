428694973956d2c67f85dcca8593ec2b7e3b4710
Haletran <bpasquier123@gmail.com>
2025-12-05 10:51:39 +0100

    start

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import { Elysia } from "elysia";
+
+
+const app = new Elysia()
+		.get("/", () => "Red Tetris")
+		.ws('/game/:room', {
+			open(ws) {
+				const { room } = ws.data.params
+				ws.subscribe(room)
+				console.log(`Player connected to ${room}`)
+			},
+			message(ws, message) {
+				const data = JSON.parse(message)
+				console.log('message from ', ws.data.params.room, data)
+			},
+			close(ws) {
+				console.log('Disconnected', ws.data.params.room)
+		}
+})
+		
+
+app.listen(3000)
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
